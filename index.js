96083796bc7f8a524c505a337364df07450d5ce4
akshay pawar <akshayp1754@gmail.com>
2025-03-08 19:09:47 +0530

    seed db removed

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const userRoutes = require("./routes/user");
 const imageRoutes = require("./routes/image");
 const cors = require("cors");
 
-const axios = require("axios");
-const FormData = require("form-data");
-
 const app = express();
 
 app.use(cors());
@@ -19,58 +16,6 @@ const PORT = process.env.PORT || 5001;
 
 app.get("/", (req, res) => {
   res.json({ greetings: "Hare Krishna" });
-});
-
-
-
-app.get("/seed-db", async (req, res) => {
-  const imageCount = 15;
-  const imageWidths = Array.from(
-    { length: imageCount },
-    () => Math.floor(Math.random() * 1000) + 200
-  );
-  const imageHeights = Array.from(
-    { length: imageCount },
-    () => Math.floor(Math.random() * 1000) + 200
-  );
-  const imageUrls = Array.from(
-    { length: imageCount },
-    (_, i) => `https://picsum.photos/${imageWidths[i]}/${imageHeights[i]}`
-  );
-
-  const dataArr = [];
-  for (let i = 0; i < imageCount; i++) {
-    const response = await axios.get(imageUrls[i], {
-      responseType: "arraybuffer",
-    });
-    const buffer = Buffer.from(response.data, "binary");
-    const fd = new FormData();
-    fd.append(`image`, buffer, { filename: `image-${i}.jpg` });
-    fd.append(`title`, `Random Image ${i}`);
-    dataArr.push(fd);
-  }
-
-  const postPromises = [];
-  for (let i = 0; i < imageCount; i++) {
-   const fd = dataArr[i];
-   postPromises.push(
-    axios.post("http://localhost:5001/api/images", fd, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-        Authorization: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJ0ZXN0QGdtYWlsLmNvbSIsImZpcnN0TmFtZSI6InRlc3QiLCJsYXN0TmFtZSI6InVzZXIiLCJpYXQiOjE3NDE0MzcyMjYsImV4cCI6MTc0MTUyMzYyNn0.6c4grzFiq8Jdbfgxgw4zzhqqXql0qSgUUNuxkrVDbN8`,
-      },
-    })
-  );
-  }
-
-  Promise.all(postPromises)
-    .then(() => {
-      res.send("Posts created successfully");
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error creating posts");
-    });
 });
 
 sequelize
